b4b0ca4a79116c6491c240090f50afb8a8263975
John Nicholas <john.nicholas@dinoroar.io>
2019-07-06 08:57:54 +0100

    fixed naming cinsonsistencies

--- tennis/TennisKatta/TennisKattaSpec/Tests.fs
+++ tennis/TennisKatta/TennisKattaSpec/Tests.fs
@@ -22,15 +22,15 @@ type ScoreState =
 
 let initial_state = EarlyGame { playerA = Love; playerB = Love }
 
-let score state =
-    let early_to_string player_score = match player_score with
+let getScore state =
+    let earlyToString player_score = match player_score with
                                        | Love -> "L"
                                        | Fifteen -> "15"
                                        | Thirty -> "30"
                                        | Fourty -> "40"
                                        | Won -> "unsupported"
 
-    let advantage_to_string score =
+    let advantageToString score =
         match score with
         | AdvantageA -> "Adv PlayerA"
         | AdvantageB -> "Adv PlayerB"
@@ -39,30 +39,30 @@ let score state =
         | WonB -> "Player B Won"
 
     match state with
-    | EarlyGame s -> (early_to_string s.playerA) + "-" + (early_to_string s.playerB)
-    | AdvantageGame s -> advantage_to_string s.score
+    | EarlyGame s -> (earlyToString s.playerA) + "-" + (earlyToString s.playerB)
+    | AdvantageGame s -> advantageToString s.score
 
 let handle command state =
-    let transition_to_advantage_game state =
+    let transitionToDeuce state =
         match state.playerA = Fourty && state.playerB = Fourty with
         | true -> AdvantageGame { score = Deuce }
         | false -> EarlyGame state
 
-    let set_playerA_won state =
+    let transitionToPlayerAWon state =
         match state with
         | EarlyGame s -> match s.playerA = Won with
                          | true -> AdvantageGame { score = WonA }
                          | false -> EarlyGame s
         | AdvantageGame _ -> state
 
-    let set_playerB_won state =
+    let transitionToPlayerBWon state =
         match state with
         | EarlyGame s -> match s.playerB = Won with
                          | true -> AdvantageGame { score = WonB }
                          | false -> EarlyGame s
         | AdvantageGame _ -> state
 
-    let increment_score score =
+    let incrementScore score =
         match score with
         | Love -> Fifteen
         | Fifteen -> Thirty
@@ -70,15 +70,15 @@ let handle command state =
         | Fourty -> Won
         | Won -> raise (WinScoreExceeded("Player A score exceeded win condition"))
 
-    let earlyGameScore command state =
+    let scoreEarlyGame command state =
         match command with
-        | PlayerAScores -> { state with playerA = (increment_score state.playerA) }
-        | PlayerBScores -> { state with playerB = (increment_score state.playerB) }
-        |> transition_to_advantage_game
-        |> set_playerA_won
-        |> set_playerB_won
+        | PlayerAScores -> { state with playerA = (incrementScore state.playerA) }
+        | PlayerBScores -> { state with playerB = (incrementScore state.playerB) }
+        |> transitionToDeuce
+        |> transitionToPlayerAWon
+        |> transitionToPlayerBWon
 
-    let handleAdvantage command state =
+    let scoreAdvantage command state =
         match state.score with
         | Deuce -> match command with
                    | PlayerAScores -> { state with score = AdvantageA }
@@ -92,18 +92,18 @@ let handle command state =
         | _ -> raise (PhaseHandlingError("attempting to score game that isnt in advantage phase using advantage phase rules"))
         
     match state with
-    | EarlyGame s -> earlyGameScore command s
-    | AdvantageGame s -> AdvantageGame(handleAdvantage command s)
+    | EarlyGame s -> scoreEarlyGame command s
+    | AdvantageGame s -> AdvantageGame(scoreAdvantage command s)
 
 
 [<Fact>]
 let ``Score is L-L by default``() =
-    Assert.Equal("L-L", score initial_state)
+    Assert.Equal("L-L", getScore initial_state)
 
 [<Fact>]
 let ``Score is 15-L when player one scores``() =
     let state = initial_state |> handle (PlayerAScores)
-    Assert.Equal("15-L", score state)
+    Assert.Equal("15-L", getScore state)
 
 [<Fact>]
 let ``Score is 15-15 when player one scores and player 2 scores``() =
@@ -111,7 +111,7 @@ let ``Score is 15-15 when player one scores and player 2 scores``() =
                 |> handle (PlayerAScores)
                 |> handle (PlayerBScores)
 
-    Assert.Equal("15-15", score state)
+    Assert.Equal("15-15", getScore state)
 
 [<Fact>]
 let ``Score is 30-15 when player one scores twice and player 2 scores once``() =
@@ -120,7 +120,7 @@ let ``Score is 30-15 when player one scores twice and player 2 scores once``() =
                 |> handle (PlayerBScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("30-15", score state)
+    Assert.Equal("30-15", getScore state)
 
 [<Fact>]
 let ``Score is 40-15 when player one scores 3 times and player 2 scores once``() =
@@ -130,7 +130,7 @@ let ``Score is 40-15 when player one scores 3 times and player 2 scores once``()
                 |> handle (PlayerAScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("40-15", score state)
+    Assert.Equal("40-15", getScore state)
 
 [<Fact>]
 let ``Score is PlayerAWins when player one scores 4 times and player 2 scores once``() =
@@ -141,7 +141,7 @@ let ``Score is PlayerAWins when player one scores 4 times and player 2 scores on
                 |> handle (PlayerAScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("Player A Won", score state)
+    Assert.Equal("Player A Won", getScore state)
 
 [<Fact>]
 let ``Score is PlayerBWon when player two scores 4 times and player 1 scores once``() =
@@ -152,7 +152,7 @@ let ``Score is PlayerBWon when player two scores 4 times and player 1 scores onc
                 |> handle (PlayerBScores)
                 |> handle (PlayerBScores)
 
-    Assert.Equal("Player B Won", score state)
+    Assert.Equal("Player B Won", getScore state)
 
 [<Fact>]
 let ``Score is deuce when it is 40-40``() =
@@ -164,7 +164,7 @@ let ``Score is deuce when it is 40-40``() =
                 |> handle (PlayerBScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("Deuce", score state)
+    Assert.Equal("Deuce", getScore state)
 
 [<Fact>]
 let ``Score is advantage player a  when it is player A scores after deuce``() =
@@ -177,7 +177,7 @@ let ``Score is advantage player a  when it is player A scores after deuce``() =
                 |> handle (PlayerAScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("Adv PlayerA", score state)
+    Assert.Equal("Adv PlayerA", getScore state)
 
 [<Fact>]
 let ``Score is advantage player b  when it is player b scores after deuce``() =
@@ -190,7 +190,7 @@ let ``Score is advantage player b  when it is player b scores after deuce``() =
                 |> handle (PlayerAScores)
                 |> handle (PlayerBScores)
 
-    Assert.Equal("Adv PlayerB", score state)
+    Assert.Equal("Adv PlayerB", getScore state)
 
 [<Fact>]
 let ``Score is deuce when player A scores after player B advantage``() =
@@ -204,7 +204,7 @@ let ``Score is deuce when player A scores after player B advantage``() =
                 |> handle (PlayerBScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("Deuce", score state)
+    Assert.Equal("Deuce", getScore state)
 
 [<Fact>]
 let ``Player A wins when scoring after advantage``() =
@@ -218,7 +218,7 @@ let ``Player A wins when scoring after advantage``() =
                 |> handle (PlayerAScores)
                 |> handle (PlayerAScores)
 
-    Assert.Equal("Player A Won", score state)
+    Assert.Equal("Player A Won", getScore state)
 
 [<Fact>]
 let ``Player B wins when scoring after advantage``() =
@@ -232,6 +232,6 @@ let ``Player B wins when scoring after advantage``() =
                 |> handle (PlayerBScores)
                 |> handle (PlayerBScores)
 
-    Assert.Equal("Player B Won", score state)
+    Assert.Equal("Player B Won", getScore state)
 
 
